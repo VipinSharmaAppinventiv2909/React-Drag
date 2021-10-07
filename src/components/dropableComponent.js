0546c21124f904d5bb8d214d5796dc5d9eb2a6da
dushyant-appinventiv <dushyant.goyal@appinventiv.com>
2021-10-07 12:16:10 +0530

    removed multiple droppable, added multiple task to single draggable

--- src/components/dropableComponent.js
+++ src/components/dropableComponent.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Draggable, Droppable } from "react-beautiful-dnd";
 import Task from "./task";
 import styled from "styled-components";
+import "./droppableComponent.css";
 
 const Container = styled.div`
  margin: 8px;
@@ -13,12 +14,75 @@ const Container = styled.div`
  
 `;
 
-function DropableComponent({ id, taskKeys }) {
+function getTask(value, key) {
+  const taskArray = [];
+  for (let i = 0; i < value; i++) {
+    taskArray.push(<Task key={i} value={key} />);
+  }
+  // console.log("TaskArray: ", taskArray);
+  return taskArray;
+}
+
+function DropableComponent({ id, taskKeys, setArray, updatedArray }) {
+  const [draggableArray, setDraggableArray] = useState([]);
+
+  // mouting , update, unmounting
+  useEffect(() => {
+    if (draggableArray.length) {
+      console.log("in useEffect after draggablearray");
+      setArray(draggableArray);
+    }
+  }, [draggableArray]);
+
+  function getItemsForDraggable(taskKeys) {
+    const map1 = new Map();
+    taskKeys.map((item) => {
+      if (!map1.has(item)) {
+        map1.set(item, 1);
+      } else {
+        map1.set(item, map1.get(item) + 1);
+      }
+    });
+    const draggableArr = [];
+    let i = 0;
+    // key = 1,2,3,4,5 value = 1-> count, 2-> count ,.....
+    for (const [key, value] of map1) {
+      console.log(key, value);
+      draggableArr.push(
+        <Draggable
+          className={"draggableStyle"}
+          key={key}
+          draggableId={JSON.stringify(key)}
+          index={i}>
+          {(provided) => (
+            <div
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}
+              ref={provided.innerRef}>
+              {getTask(value, key)}
+            </div>
+          )}
+        </Draggable>
+      );
+      i++;
+    }
+    setDraggableArray(draggableArr);
+    // console.log("Draggable arr: ", draggableArr);
+    // return draggableArr;
+  }
+
+  useEffect(() => {
+    getItemsForDraggable(taskKeys);
+  }, []);
+
   return (
     <Droppable droppableId={id}>
       {(provided) => (
         <Container {...provided.droppableProps} ref={provided.innerRef}>
-          {taskKeys?.map((item, index) => {
+          {/* {getItemsForDraggable(taskKeys)} */}
+          {/* {draggableArray?.length ? draggableArray : null} */}
+          {updatedArray?.length ? updatedArray : null}
+          {/* {taskKeys?.map((item, index) => {
             return (
               <Draggable
                 key={item}
@@ -29,7 +93,7 @@ function DropableComponent({ id, taskKeys }) {
                 )}
               </Draggable>
             );
-          })}
+          })} */}
           {provided.placeholder}
         </Container>
       )}
